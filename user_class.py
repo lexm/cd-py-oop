879fcc35d87e1f38bf0c2e06a0ea5ef102a46a52
Lex Myers <lex.myers@gmail.com>
2017-09-06 15:19:31 -0700

    added extras to user class

--- user_class.py
+++ user_class.py
@@ -1,14 +1,28 @@
+# instantiate class User
 class User(object):
+    # this method to run every time a new object is instantiated
     def __init__(self, name, email):
+	# instance attributes 
         self.name = name
         self.email = email
-        self.logged = False
+        self.logged = True
+    # login method changes the logged status for a single instance (the instance calling the method)
     def login(self):
         self.logged = True
         print self.name + " is logged in."
         return self
+    # logout method changes the logged status for a single instance (the instance calling the method)
+    def logout(self):
+        self.logged = False
+        print self.name + " is not logged in"
+        return self
+    # print name and email of the calling instance
+    def show(self):
+        print "My name is {}. You can email me at {}".format(self.name, self.email)
+        return self
 user1 = User("Anna","anna@anna.com")
-print user1.name
+user1.show()
+user1.logout()
+user1.show()
 print user1.logged
-print user1.email
 
