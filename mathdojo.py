b5d0bdb7091a95cad3a3703493fb8aaa3e93cecf
Lex Myers <lex.myers@gmail.com>
2017-09-06 15:43:34 -0700

    part 3 of mathdojo

--- mathdojo.py
+++ mathdojo.py
@@ -3,7 +3,7 @@ class MathDojo(object):
         self.result = 0
     def add(self, *args):
         for arg in args:
-            if type(arg) is list:
+            if type(arg) is list or type(arg) is tuple:
                 for item in arg:
                     self.result += item
             else:
@@ -11,7 +11,7 @@ class MathDojo(object):
         return self
     def subtract(self, *args):
         for arg in args:
-            if type(arg) is list:
+            if type(arg) is list or type(arg) is tuple:
                 for item in arg:
                     self.result -= item
             else:
@@ -22,4 +22,4 @@ md = MathDojo()
 print md.add(2).add(2,5).subtract(3,2).result
 
 md2 = MathDojo()
-print md.add([1], 3,4).add([3,5,7,8], [2,4.3,1.25]).subtract(2, [2,3], [1.1,2.3]).result
+print md.add([1], 3,4).add([3,5,7,8], (2,4.3,1.25)).subtract(2, (2,3), [1.1,2.3]).result
